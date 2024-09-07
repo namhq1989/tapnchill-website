12d192cc1170e3275a05221c1f7441bf43a8c8f7
Nam HQ <namhq.1989@gmail.com>
2024-09-07 21:56:44 +0700

    feat(web): optimize page

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor' // All modules from node_modules will be in 'vendor'
+          }
+        },
+      },
     },
   },
 })
